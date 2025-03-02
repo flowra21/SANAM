baa7f2df9b318414438bed036208e9925345092b
jwan <jwanfaiisal@outlook.com>
2025-03-02 11:33:19 +0300

    Initial Commit

---
+++ RedFlag2Tests/RedFlag2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  RedFlag2Tests.swift
+//  RedFlag2Tests
+//
+//  Created by Jwan Faisal Alameer on 02/09/1446 AH.
+//
+
+import Testing
+@testable import RedFlag2
+
+struct RedFlag2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
